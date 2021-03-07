60ee677ca88b2e6266cf35bc085576608c6ec02f
Mateusz <mateuszklonica@gmail.com>
2021-03-07 22:04:28 +0100

    test commit

---
+++ FinishedTasks/sources/task_01/gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch('*.scss', gulp.series(['sass']));
+});
---
+++ FinishedTasks/sources/task_02/imgConvert/Readme.txt
@@ -0,0 +1,4 @@
+//Images was convert to jpeg2000 or webp format for better performance and to reduce size itself
+// JPEG 2000-iOS and Safari
+//JPEG XR- IE
+// WebP - for most browser
